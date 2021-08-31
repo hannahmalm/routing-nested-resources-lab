5c697be03c122148ea5adabe9765b780428a495b
Hannah Malm <hannahemalm616@gmail.com>
2021-08-30 21:38:38 -0400

    commit tree ce1c0092c162b22963454b29da67f941b9aa4dc9

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,10 +1,37 @@
 class SongsController < ApplicationController
   def index
+    #@songs = Song.all
+    #allow the songs#index and songs#show actions to handle a valid song for the artist
+    #in the songs index action, if the artist cant be found, redirect to the index of artists, and set a flash[:alert]
+    #if somone passes in the artist id to index (index/:id)
+    #find the artist id by the id params if there is no artist id, then return the alert
+    #else if there is an id, return the songs, else if there are no params or fileters, return all the songs
+    if params[:artist_id]
+      @artist = Artist.find_by(id: params[:artist_id])
+      if @artist.nil?
+        redirect_to artists_path, alert: "Artist not found"
+      else 
+        @songs = @artist.songs
+      end 
+    else 
     @songs = Song.all
+    end 
   end
 
   def show
-    @song = Song.find(params[:id])
+    #@song = Song.find(params[:id])
+    #songs#show action, if the song cant be found for a given artist, redirect to the index of the artist's songs and set a flash[:alert]
+    #alert of "Song not found"
+    #show artist_id, if @song.nil, redirect again to the artist song path and alert song not found
+    if params[:artist_id]
+      @artist = Artist.find_by(id: params[:artist_id])
+      @song = @artist.songs.find_by(id: params[:id])
+      if @song.nil?
+        redirect_to artist_songs_path(@artist), alert: "Song not found"
+      end 
+    else 
+      @song = Song.find(params[:id])
+    end 
   end
 
   def new
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
-  resources :artists
+  resources :artists do #parent
+    resources :songs, only: [:index, :show] #child - index and show controllers 
+  end 
   resources :songs
-end
+ end
+  #create nested resource routes to show all songs for an artist
+  #restrict nested sogns routes to index and show actions 
+  #artist = parent, songs = child
+  #/artists/1/songs - show all songs for an artist
+  #/artists/1/songs/1 - Show individual songs for the artist 
+
+  
+
